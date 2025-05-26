4b4b1635098c05cd8acc5b03d91dc51ccc5a2dde
khaymanii <kennethakpo61@gmail.com>
2025-05-26 11:50:57 +0100

    fix

--- src/components/WhatsappIcon.tsx
+++ src/components/WhatsappIcon.tsx
@@ -6,7 +6,7 @@ const StickyWhatsappIcon = () => {
       href="https://wa.me/1234567890" // Replace with your WhatsApp number
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-5 right-5 bg-purple-500 text-white p-4 rounded-full shadow-lg hover:bg-purple-700 transition duration-300 ease-in-out"
+      className="fixed bottom-5 right-5 bg-white text-black p-4 rounded-full shadow-lg hover:bg-purple-700 transition duration-300 ease-in-out"
     >
       <IoChatboxOutline size={40} />
     </a>
